7d6cdb8207e3212e79681a5dffb080660549717c
barcon <12259909+barcon@users.noreply.github.com>
2025-10-21 21:50:34 +0200

    Working on Build System

--- premake5.lua
+++ premake5.lua
@@ -34,37 +34,3 @@ project "dive"
 		architecture "x86_64"     
 		defines { "NDEBUG" }
 		optimize "Speed"
-		
-workspace "diveCL"
-	configurations { "ReleaseCL" }
-	location "build"	
-
-project "diveCL"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++17"
-	objdir "%{cfg.location}/obj/%{cfg.platform}_%{cfg.buildcfg}"	
-
-	targetdir "build/%{cfg.buildcfg}"
-	includedirs { "../utils/src" }
-	includedirs { "../logger/src" }
-	includedirs { "../eilig/src" }	
-	includedirs { "../basis/src" }
-	includedirs { "../quadrature/src" }
-	includedirs { "../nodes/src" }
-	includedirs { "../kdtree/src" }
-	includedirs { "../interpolation/src" }
-	includedirs { "../values/src" }
-	includedirs { "../material/src" }
-	includedirs { "../thread-pool/include" }
-	includedirs { "../gmsh/include" }	
-
-	files { "src/**.hpp", "src/**.cpp" }
-
-	filter "configurations:ReleaseCL"
-		architecture "x86_64"
-		defines { "NDEBUG", "EILIG_ENABLE_OPENCL" }
-		optimize "Speed"
-		
-		includedirs { "../club/src" }	
-		includedirs { "../opencl/inc" }
