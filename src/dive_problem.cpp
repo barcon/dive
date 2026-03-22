0ada6498de1ce85e11c6c251140f58a8319d3216
barcon <12259909+barcon@users.noreply.github.com>
2026-03-22 09:10:43 +0100

    Working on performance

--- src/dive_problem.cpp
+++ src/dive_problem.cpp
@@ -109,7 +109,7 @@ namespace dive
 
 			Sparse global(problem1->GetTotalDof(), problem2->GetTotalDof());	
 
-			NumberProcessors numberProcessors = std::thread::hardware_concurrency();
+			NumberProcessors numberProcessors = std::thread::hardware_concurrency() > 1 ? std::thread::hardware_concurrency() - 1 : 1;
 			NumberElements numberElements1 = elements1.size();
 			Number numberTasks = (numberElements1 % taskSize) == 0 ? (numberElements1 / taskSize) : (numberElements1 / taskSize) + 1;
 
